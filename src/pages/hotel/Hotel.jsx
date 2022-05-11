c48413251db33a4de5556ab60fc6c6e171aff183
rashedul alam <rashedulalam362@gmail.com>
2022-05-11 23:02:05 +0600

    final

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -30,20 +30,41 @@ const Hotel = () => {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
     },
   ];
+
+  const handleOpen = (i) => {
+    setSlideNumber(i);
+    setOpen(true);
+
+  }
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if (direction === "l") {
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+    } else {
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+
+    setSlideNumber(newSlideNumber)
+  };
   return (
     <div>
       <Navbar/>
       <Header />
       <div className="hotelContainer">
-        <div className="slider">
+        {open && <div className="slider">
+        
           <FontAwesomeIcon 
-
-          icon={faCircleXmark}/>
+           icon={faCircleXmark} className="close" onClick={()=>setOpen(false)}/>
           <FontAwesomeIcon 
-          icon={faCircleArrowLeft}/>
+          icon={faCircleArrowLeft} className="arrow" onClick={()=>handleMove('l')}/>
+          <div className="sliderWrapper">
+            <img src={photos[slideNumber].src}
+            className="sliderImg" alt="" />
+          </div>
           <FontAwesomeIcon 
-          icon={faCircleArrowRight}/>
-        </div>
+          icon={faCircleArrowRight} className="arrow"  onClick={()=>handleMove('r')}/>
+        </div>}
         <div className="hotelWrapper">
         <button className="bookNow">Reserve or Book Now!</button>
         <h1 className="hotelTitle">Tower Street Apartments</h1>
@@ -61,7 +82,7 @@ const Hotel = () => {
            {photos.map((photo, i) => (
               <div className="hotelImgWrapper" key={i}>
                 <img
-                  onClick={() => setSlideNumber(i)}
+                  onClick={() =>handleOpen(i)}
                   src={photo.src}
                   alt=""
                   className="hotelImg"
